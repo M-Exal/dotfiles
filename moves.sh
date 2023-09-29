8a2e1863fd99fa0e7c0ec0bcd854c07793045c62
Alexis Blanc-Feuillet <alexis.blancfeuillet@gmail.com>
2023-09-29 16:51:26 +0200

    added moves of config file script

---
+++ moves.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cp i3_config/i3/config ~/.config/i3
+cp i3_config/i3status/i3status.conf ~/.config/i3status/
+cp picom_config/picom.conf ~/.config/picom/
+cp rofi/config ~/.config/rofi/
+cp zsh/oh-my-zsh.sh ~/.oh-my-zsh/
+cp zsh/aliases.sh ~/.oh-my-zsh/custom/
+cp zsh/zsh-* ~
