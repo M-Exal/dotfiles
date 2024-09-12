a2eb971ebefdc6951ebfc9862c8e631e220d4ed5
Alexis <alexis.blancfeuillet@gmail.com>
2024-09-12 23:40:58 +0200

    init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -34,7 +34,6 @@ require("lazy").setup({
       }
     end
   },
-  --{'feline-nvim/feline.nvim'},
   {
   'nvimdev/dashboard-nvim',
   event = 'VimEnter',
@@ -44,10 +43,6 @@ require("lazy").setup({
     }
   end,
   dependencies = { {'nvim-tree/nvim-web-devicons'}}
-  },
-  {
-    'neoclide/coc.nvim',
-    branch = 'release'
   },
 })
 
@@ -76,25 +71,6 @@ vim.g.coc_global_extensions = {
   'coc-angular'
 }
 
-
-if vim.fn.argc() == 0 then
-  vim.cmd("autocmd VimEnter * :Dashboard") --open dashboard on startup
-end
-
--- consider .es6 as js 
-vim.api.nvim_exec([[
-  autocmd BufNewFile,BufRead *.es6 set filetype=javascript
-]], false)
--- consider .scss as css
-vim.api.nvim_exec([[
-    autocmd BufNewFile,BufRead *.scss set filetype=css
-]], false)
-
--- consider .scss as css
-vim.api.nvim_exec([[
-    autocmd BufNewFile,BufRead *.rb set filetype=ruby
-]], false)
-
 -- keymaps 
 vim.keymap.set('n', '<space>w', '<cmd>write<cr>', {desc = 'Save'}) --space+s -> write
 vim.keymap.set('n', '<space>q', '<cmd>quit<cr>', {desc = 'Quit'}) --space+q -> quit
@@ -108,47 +84,6 @@ vim.keymap.set('n', '<F2>', ':tabnext<cr>')
 -- clipboard
 vim.keymap.set({'n', 'x'}, 'gy', '"+y') -- g+y -> copy
 vim.keymap.set({'n', 'x'}, 'gp', '"+p') -- g+p -> paste
-
-
--- Coc config
--- Some servers have issues with backup files, see #649
-vim.opt.backup = false
-vim.opt.writebackup = false
-
--- Having longer updatetime (default is 4000 ms = 4s) leads to noticeable
--- delays and poor user experience
-vim.opt.updatetime = 300
-
--- Always show the signcolumn, otherwise it would shift the text each time
--- diagnostics appeared/became resolved
-vim.opt.signcolumn = "yes"
-
-local keyset = vim.keymap.set
--- Autocomplete
-function _G.check_back_space()
-    local col = vim.fn.col('.') - 1
-    return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
-end
-
--- Use Tab for trigger completion with characters ahead and navigate
--- NOTE: There's always a completion item selected by default, you may want to enable
--- no select by setting `"suggest.noselect": true` in your configuration file
--- NOTE: Use command ':verbose imap <tab>' to make sure Tab is not mapped by
--- other plugins before putting this into your config
-local opts = {silent = true, noremap = true, expr = true, replace_keycodes = false}
-keyset("i", "<TAB>", 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', opts)
-keyset("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
-
--- Make <CR> to accept selected completion item or notify coc.nvim to format
--- <C-g>u breaks current undo, please make your own choice
-keyset("i", "<cr>", [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], opts)
-
-
--- Configuration de Prettier
-vim.api.nvim_exec([[
-  autocmd BufWritePre *.js,*.jsx,*.html,*.rb,*.es6 PrettierAsync
-  autocmd BufWritePre * match ExtraWhitespace /\s\+$/
-]], false)
 
 require('lualine').setup{
   theme = 'tokyonight'
@@ -166,20 +101,6 @@ vim.api.nvim_set_hl(0, 'StatusLineNC', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'LineNr', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'CursorLineNr', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'VertSplit', { bg = 'none' })
-
---vim.api.nvim_create_autocmd("BufWritePost", {
---  pattern = "*.c",
---  callback = function()
---    -- Notify when clang-format is being applied
---    print("Running clang-format...")
---
---    -- Run clang-format
---    vim.fn.system("clang-format -i " .. vim.fn.expand("%"))
---
---    -- Force reload of the current buffer
---    vim.api.nvim_command('checktime')
---  end,
---})
 
 vim.api.nvim_create_autocmd("BufWritePost", {
   pattern = "*.c",
